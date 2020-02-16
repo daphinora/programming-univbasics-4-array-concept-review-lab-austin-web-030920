6e86a389d607eb0e7a0a2cc845bffa8d59d245c7
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-16 01:48:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0
    while counter < array.length do
-    array.index.times(counter)
+    array.index(value_to_find)
   end
    counter += 1
    nil
