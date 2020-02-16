fedc0928ef33a024d17d36e0ec90b5159ff90191
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-16 01:46:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,9 @@ def find_element_index(array, value_to_find)
   counter = 0
    while counter < array.length do
     array.index(value_to_find)
-    counter += 1
+   
   end
-  nil
+  nil counter += 1
 end
 
 def find_max_value(array)
