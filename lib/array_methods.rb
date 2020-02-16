9220dd0bffdcff21834e110c7c83d70a4428c060
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-16 02:18:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,7 +24,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  loser = array[0]
+  array.length.times do |index|
+    if array[index] < loser
+      loser = array[index]
+    end
+  end
+  loser
 end
 
 
