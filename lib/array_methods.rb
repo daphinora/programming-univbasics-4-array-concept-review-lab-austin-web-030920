5e9b5cc45b6191e2b3e29534d7561ad5ae1a6460
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-16 01:55:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,6 +13,9 @@ def find_element_index(array, value_to_find)
   array.length.times do |counter|
     if array[counter] == value_to_find
       return counter
+    end
+    nil
+  end
 
 
 def find_max_value(array)
